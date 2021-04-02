844b5865549fb97093a2e5e5f5a23b165c87d317
Pawarit Thareechan <root@lckyn.com>
2021-04-02 20:35:34 +0700

    add latex slowly download

---
+++ install_latex.sh
@@ -0,0 +1,5 @@
+brew install --cask miktex-console
+brew install --cask mactex
+
+tlmgr install "package"
+tlmgr install standalone preview doublestroke relsize fundus-calligra wasysym physics dvisvgm.x86_64-darwin dvisvgm rsfs wasy cm-super
